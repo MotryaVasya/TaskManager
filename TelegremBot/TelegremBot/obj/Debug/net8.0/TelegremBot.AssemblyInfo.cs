bd26dd3aa02e237b1c79e6527656703812f217f2
Vasya Motrya <motravasa5@gmail.com>
2024-11-26 23:54:50 +0700

    fdsf

--- TelegremBot/TelegremBot/obj/Debug/net8.0/TelegremBot.AssemblyInfo.cs
+++ TelegremBot/TelegremBot/obj/Debug/net8.0/TelegremBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelegremBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+995dfbdcf5d43f0e2b55e370455b5831e9a5e630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4952d6015febb5014c5ed74692ab101d826d8df9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegremBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelegremBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
